2f930d9d4787ae0b3a23fbf8b9e95218362e0e30
EPAM\devendra_singh2 <devendra_singh2@epam.com>
2025-01-10 17:37:32 +0530

    added jenkins task for declarative pipeline

---
+++ jenkinsdemo/src/test/java/epam/AppTest.java
@@ -0,0 +1,19 @@
+package epam;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+
+    @Test
+    public void myDemoTest() {
+        Assert.assertTrue(true);
+        System.out.println("Test Passed");
+    }
+   
+}
